9d6d4be75d74f8b68774eca712ea218424cfd78c
u6yuvi <u6yuvi@gmail.com>
2024-06-24 23:57:43 -0700

    add exercice-4 template

---
+++ basic/exercise4/session4.py
@@ -0,0 +1,15 @@
+import random
+from decimal import *
+
+class Qualean:
+  '''
+  Yous description
+  '''
+  def __init__(self):
+    pass
+  
+  # defining object representation
+  def __repr__(self):
+    return 'Proper text representing the class'
+  
+  # your other functions
